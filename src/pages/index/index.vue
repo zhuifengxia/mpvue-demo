5d86aef2a676a0f8c9baa3404df8456bacfaf03b
momo <xirizhifeng@163.com>
2019-12-26 13:57:22 +0800

    banner component

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,17 +2,23 @@
   <div class="home">
     <SearchBar :disabled="false" @onClick="onSearchBarClick"></SearchBar>
     <home-card/>
+    <home-banner img="http://www.youbaobao.xyz/book/res/bg.jpg" title="mpvue2.0实战多端小程序课程上线啦" sub-title="立即体验"
+                 @onClick="onBannerClick"></home-banner>
   </div>
 </template>
 
 <script>
   import SearchBar from '../../components/home/SearchBar'
   import HomeCard from '../../components/home/HomeCard'
+  import HomeBanner from '../../components/home/HomeBanner'
 
   export default {
-    components: {HomeCard, SearchBar},
+    components: {HomeBanner, HomeCard, SearchBar},
     methods: {
       onSearchBarClick () {
+      },
+      onBannerClick () {
+        console.log('banner click')
       }
     }
   }
