810a76b5d3675cd21c8ab891331ac6729181243e
momo <xirizhifeng@163.com>
2019-12-27 16:39:39 +0800

    search table component

---
+++ src/components/search/SearchTable.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="search-table-wrapper">
+    <div class="search-table-inner">
+      <div class="search-table-book" v-for="(book,index) in data" @click="onClick(book)">
+        <div class="book-img-wrapper">
+          <div class="book-img">
+            <image-view :src="book.cover"></image-view>
+          </div>
+        </div>
+        <div class="book-info-wrapper">
+          <div class="book-title">{{book.title}}</div>
+          <div class="book-author">{{book.author}}</div>
+          <div class="book-category">{{book.categoryText}}</div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+<script>
+  import ImageView from '../base/ImageView'
+
+  export default {
+    components: {ImageView},
+    props: {
+      data: Array
+    },
+    methods: {
+      onClick (book) {
+        this.$emit('onClick', book)
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .search-table-wrapper {
+    padding: 0 16px;
+    .search-table-inner {
+      .search-table-book {
+        display: flex;
+        align-items: center;
+        margin: 14.5px 0;
+
+        .book-img-wrapper {
+          .book-img {
+            width: 47px;
+            height: 68.5px;
+          }
+        }
+
+        .book-info-wrapper {
+          width: 80%;
+          margin-left: 15.5px;
+
+          .book-title {
+            width: 100%;
+            color: #333;
+            font-size: 16px;
+            line-height: 22.5px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+          }
+
+          .book-author {
+            color: rgba(0,0,0,.65);
+            font-size: 13px;
+            line-height: 18px;
+            margin-top: 5px;
+            font-weight: 500;
+          }
+
+          .book-category {
+            color: rgba(0,0,0,.45);
+            font-size: 13px;
+            line-height: 18px;
+          }
+        }
+      }
+
+    }
+  }
+</style>
--- src/pages/search/search.vue
+++ src/pages/search/search.vue
@@ -1,12 +1,20 @@
 <template>
-  <tag-group :value="tags" header-text="热门搜索" btn-text="换一批" @onTagClick="onTagClick"
-             @onBtnClick="onBtnClick"></tag-group>
+  <div>
+    <tag-group :value="tags" header-text="热门搜索" btn-text="换一批" @onTagClick="onTagClick"
+               @onBtnClick="onBtnClick"></tag-group>
+    <search-item icon="apps-o" title="计算机科学与技术" sub-title="类别"></search-item>
+
+    <search-table :data="list"></search-table>
+  </div>
+
 </template>
 <script>
   import TagGroup from '../../components/base/TagGroup'
+  import SearchItem from '../../components/search/SearchItem'
+  import SearchTable from '../../components/search/SearchTable'
 
   export default {
-    components: {TagGroup},
+    components: {SearchTable, TagGroup, SearchItem},
     data () {
       return {
         tags: [
@@ -16,6 +24,47 @@
           'ddd',
           '测试测试测试测试',
           'mmm'
+        ],
+        list: [
+          {
+            'id': 225,
+            'fileName': '2016_Book_MicrofinanceEUStructuralFundsA',
+            'cover': 'https://www.youbaobao.xyz/book/res/img/Economics/2016_Book_MicrofinanceEUStructuralFundsA.jpeg',
+            'title': 'Microfinance, EU Structural Funds and Capacity Building for Managing Authorities',
+            'author': 'Giovanni Nicola Pes',
+            'publisher': 'Palgrave Macmillan',
+            'bookId': '2016_Book_MicrofinanceEUStructuralFundsA',
+            'category': 3,
+            'categoryText': 'Economics',
+            'language': 'en',
+            'rootFile': 'OEBPS/9781137536013.opf'
+          },
+          {
+            'id': 88,
+            'fileName': '2018_Book_BetweenMobilityAndMigration',
+            'cover': 'https://www.youbaobao.xyz/book/res/img/SocialSciences/978-3-319-77991-1_CoverFigure.jpg',
+            'title': 'Between Mobility and Migration',
+            'author': 'Peter Scholten',
+            'publisher': 'Springer International Publishing',
+            'bookId': '2018_Book_BetweenMobilityAndMigration',
+            'category': 2,
+            'categoryText': 'SocialSciences',
+            'language': 'en',
+            'rootFile': 'OEBPS/package.opf'
+          },
+          {
+            'id': 24,
+            'fileName': '2018_Book_SecurityInComputerAndInformati',
+            'cover': 'https://www.youbaobao.xyz/book/res/img/ComputerScience/978-3-319-95189-8_CoverFigure.jpg',
+            'title': 'Security in Computer and Information Sciences',
+            'author': 'Erol Gelenbe',
+            'publisher': 'Springer International Publishing',
+            'bookId': '2018_Book_SecurityInComputerAndInformati',
+            'category': 1,
+            'categoryText': 'ComputerScience',
+            'language': 'en',
+            'rootFile': 'OEBPS/package.opf'
+          }
         ]
       }
     },
