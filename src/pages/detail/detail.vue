9527f7c03aabcd94c9a5e6a471dac8c0c5e8a4e3
momo <xirizhifeng@163.com>
2019-12-28 17:30:40 +0800

    bug fixed

--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -1,25 +1,43 @@
 <template>
   <div>
-    <detail-book :book="book"></detail-book>
-
-    <detail-stat :readers="book.readers" :readerNum="book.readerNum" :rankNum="book.rankNum"
-                 :rankAvg="book.rankAvg"></detail-stat>
-
-    <detail-rate :rate-value="book.rateValue" @onRateChange="onRateChange"></detail-rate>
-
-    <detail-contents :contents="contents" @readBook="readBook"></detail-contents>
-
-    <detail-bottom :isInShelf="isInShelf" @handleShelf="handleShelf" @readBook="readBook"></detail-bottom>
+    <DetailBook :book="book"/>
+    <DetailStat
+      :readers="book.readers"
+      :readerNum="book.readerNum"
+      :rankNum="book.rankNum"
+      :rankAvg="book.rankAvg"
+    />
+    <DetailRate
+      :rate-value="book.rateValue"
+      @onRateChange="onRateChange"
+    />
+    <DetailContents
+      :contents="contents"
+      @readBook="readBook"
+    />
+    <DetailBottom
+      :is-in-shelf="isInShelf"
+      @handleShelf="handleShelf"
+      @readBook="readBook"
+    />
   </div>
 </template>
+
 <script>
   import DetailBook from '../../components/detail/DetailBook'
-  import {bookContents, bookDetail, bookIsInShelf, bookRankSave, bookShelfRemove, bookShelfSave} from '../../api'
-  import {getStorageSync} from '../../api/wechat'
   import DetailStat from '../../components/detail/DetailStat'
   import DetailRate from '../../components/detail/DetailRate'
   import DetailContents from '../../components/detail/DetailContents'
   import DetailBottom from '../../components/detail/DetailBottom'
+  import {
+    bookDetail,
+    bookRankSave,
+    bookContents,
+    bookShelfSave,
+    bookShelfRemove,
+    bookIsInShelf
+  } from '../../api'
+  import {getStorageSync} from '../../api/wechat'
 
   export default {
     components: {DetailBottom, DetailContents, DetailRate, DetailStat, DetailBook},
@@ -33,11 +51,9 @@
     methods: {
       handleShelf () {
         const openId = getStorageSync('openId')
-        const fileName = this.$route.query.fileName
+        const {fileName} = this.$route.query
         if (!this.isInShelf) {
-          if (openId && fileName) {
-            bookShelfSave({openId, fileName}).then(this.getBookIsInShelf)
-          }
+          bookShelfSave({openId, fileName}).then(this.getBookIsInShelf)
         } else {
           const vue = this
           mpvue.showModal({
@@ -45,81 +61,63 @@
             content: `是否确认将《${this.book.title}》移出书架？`,
             success (res) {
               if (res.confirm) {
-                if (openId && fileName) {
-                  bookShelfRemove({openId, fileName}).then(vue.getBookIsInShelf)
-                }
+                bookShelfRemove({openId, fileName}).then(vue.getBookIsInShelf)
               }
             }
           })
         }
       },
       readBook (href) {
-        const qurey = {
+        const query = {
           fileName: this.book.fileName,
           opf: this.book.opf
         }
         if (href) {
           const index = href.indexOf('/')
           if (index >= 0) {
-            qurey.navigation = href.slice(index + 1)
+            query.navigation = href.slice(index + 1)
           } else {
-            qurey.navigation = href
+            query.navigation = href
           }
         }
         if (this.book && this.book.fileName) {
           this.$router.push({
-            path: '/pages/read/mian',
-            qurey
+            path: '/pages/read/main',
+            query
           })
         }
       },
       onRateChange (value) {
         const openId = getStorageSync('openId')
-        const fileName = this.$route.query.fileName
-        if (openId && fileName) {
-          bookRankSave({
-            openId, fileName, rank: value
-          }).then(res => {
-            this.getBookDetail()
-          })
-        }
+        const {fileName} = this.$route.query
+        bookRankSave({openId, fileName, rank: value}).then(() => {
+          this.getBookDetail()
+        })
       },
       getBookDetail () {
         const openId = getStorageSync('openId')
-        const fileName = this.$route.query.fileName
+        const {fileName} = this.$route.query
         if (openId && fileName) {
-          bookDetail({
-            openId,
-            fileName
-          }).then(res => {
-            this.book = res.data.data
+          bookDetail({openId, fileName}).then(response => {
+            this.book = response.data.data
           })
         }
       },
       getBookContents () {
-        const fileName = this.$route.query.fileName
+        const {fileName} = this.$route.query
         if (fileName) {
-          bookContents({
-            fileName
-          }).then(res => {
-            this.contents = res.data.data
+          bookContents({fileName}).then(response => {
+            this.contents = response.data.data
           })
         }
       },
       getBookIsInShelf () {
         const openId = getStorageSync('openId')
-        const fileName = this.$route.query.fileName
+        const {fileName} = this.$route.query
         if (openId && fileName) {
-          bookIsInShelf({
-            openId,
-            fileName
-          }).then(res => {
-            const {data} = res.data
-            if (data.length === 0) {
-              this.isInShelf = false
-            } else {
-              this.isInShelf = true
-            }
+          bookIsInShelf({openId, fileName}).then(response => {
+            const {data} = response.data
+            data.length === 0 ? this.isInShelf = false : this.isInShelf = true
           })
         }
       }
@@ -131,6 +129,6 @@
     }
   }
 </script>
-<style lang="scss" scoped>
 
+<style lang="scss" scoped>
 </style>
